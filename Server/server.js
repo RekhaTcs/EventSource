be7c591aead70903bf8c1b91430388bc54814478
Rekha <rekha.b3@tcs.com>
2016-09-16 11:16:52 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,13 @@
 var http = require('http');
 var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Transfer-Encoding': 'chunked', 'Content-Type': 'text/event-stream' });
